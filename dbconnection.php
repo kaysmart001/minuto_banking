3aa8a310b65ea34f0e0de9f308f9828df029154a
kaysmart001 <luwoyekayode99@gmail.com>
2023-01-29 12:39:59 +0100

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,9 +1,9 @@
 <?php
-$con = mysql_connect("localhost","root","password");
+$con = mysql_connect("sql311.epizy.com","epiz_33459705","hvZgW4p2guu7");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
 
-mysql_select_db("netbanking");
+mysql_select_db("epiz_33459705_bank_db");
 ?>
